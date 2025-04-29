c247d75889077a4b116a8f2ffaaa18ef221c49d5
Murilo Augusto <140767549+Muraups@users.noreply.github.com>
2025-04-29 00:50:39 -0300

    test

--- Scripts/perfil.js
+++ Scripts/perfil.js
@@ -41,7 +41,7 @@ document.addEventListener("DOMContentLoaded", function() {
 function logout() {
     localStorage.removeItem("userData");
     window.location.href = "index.html";
-}
+};
 
 
 function toggleMenu() {
